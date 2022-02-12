f33d404e98afa617e73784d5f5c8e14bbfb718a2
laura.kutovaja <laura.kutovaja@visma.com>
2022-02-12 21:10:14 +0200

    trying out json-server to fetch data from backend (not fully working yet)

--- src/carousel.js
+++ src/carousel.js
@@ -1,5 +1,32 @@
 'use strict';
 
+const loadCarousel = async function(){
+    try {
+        const carouselContainer = document.querySelector('.carousel');
+        const carouselData = await fetch('http://localhost:3000/carousel');  
+        const data = await carouselData.json();
+
+        const carousel = `
+        ${data.map(carouselItem => {
+            return `   
+                <div class="carousel-item">
+                    <a href="${carouselItem.redirectUrl}" target="_blank">
+                        <img src="${carouselItem.imgSrc}" />
+                        <h1>${carouselItem.desc}</h1>
+                    </a>
+                </div>
+            `
+        })}`;
+
+        carouselContainer.insertAdjacentHTML('afterbegin', carousel);
+
+    } catch (error) {
+        alert(error);
+    }
+}
+
+loadCarousel();
+
 const carouselItems = document.querySelectorAll('.carousel-item');
 const leftButton = document.querySelector('.carousel-btn-left');
 const rightButton = document.querySelector('.carousel-btn-right');
