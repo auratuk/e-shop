a16d7a0ea0b37531418093b7ea7f5d382bdfdc47
laura.kutovaja <laura.kutovaja@visma.com>
2022-02-13 13:57:28 +0200

    make carousel work (add await before loadCarousel() and wrap rest of the code into function)

--- src/carousel.js
+++ src/carousel.js
@@ -6,7 +6,7 @@ const loadCarousel = async function(){
         const carouselData = await fetch('http://localhost:3000/carousel');  
         const data = await carouselData.json();
 
-        const carousel = `
+        const carouselItems = `
         ${data.map(carouselItem => {
             return `   
                 <div class="carousel-item">
@@ -16,56 +16,54 @@ const loadCarousel = async function(){
                     </a>
                 </div>
             `
-        })}`;
+        }).join('')}`;
 
-        carouselContainer.insertAdjacentHTML('afterbegin', carousel);
+        carouselContainer.insertAdjacentHTML('afterbegin', carouselItems);
 
     } catch (error) {
         alert(error);
     }
 }
 
-loadCarousel();
+const initCarousel = function() {
+    const carouselItems = document.querySelectorAll('.carousel-item');
+    const leftButton = document.querySelector('.carousel-btn-left');
+    const rightButton = document.querySelector('.carousel-btn-right');
 
-const carouselItems = document.querySelectorAll('.carousel-item');
-const leftButton = document.querySelector('.carousel-btn-left');
-const rightButton = document.querySelector('.carousel-btn-right');
-
-let currentCarouselItem = 0;
-
-const initCarousel = () => {
+    let currentCarouselItem = 0;
     transformSlides();
-}
 
-rightButton.addEventListener('click', function() {
-    if(currentCarouselItem === carouselItems.length - 1){
-        currentCarouselItem = 0;
-    } else {
-        currentCarouselItem++;
-    }
-
-    transformSlides();
-});
-
-leftButton.addEventListener('click', function() {
-    if(currentCarouselItem === 0){
-        currentCarouselItem = carouselItems.length - 1;
-    } else {
-        currentCarouselItem--;
-    }
-
-    transformSlides();
-});
-
-function transformSlides() {
-    carouselItems.forEach((carouselItem, index) => {
-        carouselItem.classList.remove('carousel-item--visible');
-        carouselItem.classList.add('carousel-item--hidden');
-
-        carouselItem.style.transform = `translateX(${100 * (index - currentCarouselItem)}%)`;
+    rightButton.addEventListener('click', function() {
+        if(currentCarouselItem === carouselItems.length - 1){
+            currentCarouselItem = 0;
+        } else {
+            currentCarouselItem++;
+        }
+    
+        transformSlides();
+    });
+    
+    leftButton.addEventListener('click', function() {
+        if(currentCarouselItem === 0){
+            currentCarouselItem = carouselItems.length - 1;
+        } else {
+            currentCarouselItem--;
+        }
+    
+        transformSlides();
     });
 
-    carouselItems[currentCarouselItem].classList.add('carousel-item--visible');
+    function transformSlides() {
+        carouselItems.forEach((carouselItem, index) => {
+            carouselItem.classList.remove('carousel-item--visible');
+            carouselItem.classList.add('carousel-item--hidden');
+    
+            carouselItem.style.transform = `translateX(${100 * (index - currentCarouselItem)}%)`;
+        });
+    
+        carouselItems[currentCarouselItem].classList.add('carousel-item--visible');
+    }
 }
 
+await loadCarousel();
 initCarousel();
