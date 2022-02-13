33cdc14c1030ce551eaa23c2e2ac670384a8c30b
laura.kutovaja <laura.kutovaja@visma.com>
2022-02-13 14:35:41 +0200

    load books from endpoint

--- src/app.js
+++ src/app.js
@@ -1,3 +1,29 @@
 'use strict';
 
 import './carousel.js';
+
+const loadBooks = async function(){
+    try {
+        const cardsContainer = document.querySelector('.cards');
+        const booksData = await fetch('http://localhost:3000/books');  
+        const data = await booksData.json();
+
+        const books = `
+        ${data.map(book => {
+            return `   
+            <div class="card">
+                <img src="${book.imgSrc}"/>
+                <div class="book-details">
+                    <h1 class="book-name"> ${book.name} </h1>
+                    <h2 class="book-author"> ${book.author} </h2>
+                </div>
+            </div>
+            `}).join('')}`;
+
+        cardsContainer.insertAdjacentHTML('afterbegin', books);
+    } catch (error) {
+        alert(error);
+    }
+}
+
+await loadBooks();
